7ecd02970bbb0fe249c1ea27cbc6f186726ead39
Camille Doré <camille.dore75@gmail.com>
2023-01-27 22:23:59 +0100

    scrapinp plusieurs billboard

--- scraper_test.py
+++ scraper_test.py
@@ -19,16 +19,22 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 # driver = webdriver.Chrome(service=service)
 
 # Navigate to the website
-driver.get("https://www.billboard.com/charts/hot-100/")
+# driver.get("https://www.billboard.com/charts/hot-100/")
+# driver.get("https://www.billboard.com/charts/billboard-global-200/")
+driver.get("https://www.billboard.com/charts/billboard-200/")
 
 # Wait for the page to load
 driver.implicitly_wait(10)
 
-iterateur = list(range(1,110,11))
-iterateur.remove(1)
+iterateur100 = list(range(1,110,11))
+iterateur100.remove(1)
+iterateur200 = list(range(1,220,11))
+iterateur200.remove(1)
 
-def scraper(list,xpath):
-    for i in range(2,111):
+# 111 ou 221
+
+def scraper(list,xpath,iterateur,nb_max):
+    for i in range(2,nb_max):
         if i not in iterateur : 
             WebElement = driver.find_element(By.XPATH,xpath%(i))
             list.append(WebElement.text)
@@ -49,12 +55,12 @@ last_week =[]
 peak_pos = []
 weeks_on_chart = []
 
-title = scraper(title,XpathTitle)
-artist = scraper(artist,XpathArtist)
-rank = scraper(rank,XpathRank)
-last_week = scraper(last_week,XpathLastWeek)
-peak_pos = scraper(peak_pos,XpathPeakPosition)
-weeks_on_chart = scraper(weeks_on_chart,XpathWeeksOnChart)
+title = scraper(title,XpathTitle,iterateur200,221)
+artist = scraper(artist,XpathArtist,iterateur200,221)
+rank = scraper(rank,XpathRank,iterateur200,221)
+last_week = scraper(last_week,XpathLastWeek,iterateur200,221)
+peak_pos = scraper(peak_pos,XpathPeakPosition,iterateur200,221)
+weeks_on_chart = scraper(weeks_on_chart,XpathWeeksOnChart,iterateur200,221)
 
 df = pd.DataFrame(list(zip(title,artist,rank,last_week,peak_pos,weeks_on_chart)),columns=['Title','Artist','Rank','Last Week','Peak Positon','Weeks on charts'])
 print(df)
