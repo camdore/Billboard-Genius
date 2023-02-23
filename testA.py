232876caf775b3160bd9e3c219c01845f5223ffa
lafkiha <adam.lafkih@edu.esiee.fr>
2023-02-23 20:09:45 +0100

    Filtres (pas encore 100% fonctionels)

--- testA.py
+++ testA.py
@@ -7,10 +7,32 @@ app = Flask(__name__)
 def index():
     data = pd.read_csv("dataframe_finale.csv", sep=";")
     if request.method == "POST":
-        query = request.form["genre"]
-        results = data.query('Genre.str.contains(@query, na=False)', engine='python')
-        return render_template("index.html",  table=results.to_html(classes="table", index=False, table_id="table"))
-    return render_template("index.html", table=data.to_html(classes="table", index=False, table_id="table"))
+        query = request.form["query"]
+         # Récupérer les colonnes sélectionnées
+        columns = []
+        if request.form.get("title"):
+            columns.append("Title")
+        if request.form.get("artist"):
+            columns.append("Artist")
+        if request.form.get("distributor"):
+            columns.append("Distributor")
+        if request.form.get("producers"):
+            columns.append("Producers")
+        if request.form.get("genre"):
+            columns.append("Genre")
+        # Filtrer les résultats en fonction des colonnes sélectionnées
+        if len(columns) > 0:
+            results = pd.DataFrame(columns=data.columns)
+            for col in columns:
+                results = results.append(data[data[col].str.contains(query, na=False)])
+        else:
+            results = data[data.apply(lambda row: row.str.contains(query, na=False)).any(axis=1)]
+        
+        # Retourner le résultat filtré sous forme de tableau HTML
+        return render_template("index.html", table=results.to_html(classes="table", index=False))
+    
+    # Retourner le tableau complet par défaut
+    return render_template("index.html", table=data.to_html(classes="table", index=False))
 
 if __name__ == "__main__":
     app.run()
