fb919684c276ec2a403adbb0fda98547a3431191
lafkiha <adam.lafkih@edu.esiee.fr>
2023-02-23 14:59:34 +0100

    Ouvre le dataframe_finale en entier sans erreur

--- testA.py
+++ testA.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    data = pd.read_csv("my_data.csv")
+    data = pd.read_csv("dataframe_finale.csv", sep=";")
     return render_template("index.html", data=data.to_html())
 
 if __name__ == "__main__":
