948e2668227cbdd61e06d160f9c741e6335610b0
Camille Doré <camille.dore75@gmail.com>
2023-02-26 02:20:57 +0100

    graph flask

--- scraper_test.py
+++ scraper_test.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time 
 import datetime
@@ -14,6 +11,7 @@ from elasticsearch.helpers import bulk
 import numpy as np
 import re
 from flask import Flask, render_template, request
+import plotly.express as px
 
 start_time = time.time()
 ##################################################### SCRAPING BILLBOARD #####################################################
@@ -403,22 +401,6 @@ if es.indices.exists('billboard')==True:
 else :
     bulk(es, generate_data(data))
 
-
-##################################################### FLASK APP ##########################################################
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        query = request.form['query']
-        # field = request.form['field']
-        field = request.form.get('field')
-        results = search(query, field)
-        return render_template('index.html', results=results)
-    else:
-        return render_template('index.html')
-
 def search(query, field):
     QUERY ={
     "query": {
@@ -449,6 +431,132 @@ def search(query, field):
     [results.append(elt['_source']) for elt in result["hits"]["hits"]]
 
     return results
+def searchinfos(query, field):
+    QUERY ={
+    "query": {
+        "bool": {
+        "must": [],
+        "filter": [
+            {
+            "bool": {
+                "should": [
+                {
+                    "match_phrase": {
+                    field : query
+                    }
+                }
+                ],
+                "minimum_should_match": 1
+            }
+            }
+        ],
+        "should": [],
+        "must_not": []
+        }
+    }
+    }
+    result = es.search(index="billboard", body=QUERY,size=4200)
+
+    results = []
+    [results.append(elt['_source']) for elt in result["hits"]["hits"]]
+
+    N_DOCS = result['hits']['total']['value']
+    infos = f"{N_DOCS} document{'s' if N_DOCS> 1 else '' } correspondent à la requête qui a pris {result['took']} ms"
+
+    return infos
+
+def graphsong_rank(query,field):
+    QUERY = {
+    "aggs": {
+        "2": {
+        "date_histogram": {
+            "field": "Date",
+            "calendar_interval": "1w",
+            "time_zone": "Europe/Paris",
+            "min_doc_count": 1
+        },
+        "aggs": {
+            "1": {
+            "avg": {
+                "field": "Rank"
+            }
+            }
+        }
+        }
+    },
+    "size": 0,
+    "_source": {
+        "excludes": []
+    },
+    "stored_fields": [
+        "*"
+    ],
+    "script_fields": {},
+    "docvalue_fields": [
+        {
+        "field": "Date",
+        "format": "date_time"
+        }
+    ],
+    "query": {
+        "bool": {
+        "must": [],
+        "filter": [
+            {
+            "bool": {
+                "should": [
+                {
+                    "match_phrase": {
+                    field : query
+                    }
+                }
+                ],
+                "minimum_should_match": 1
+            }
+            }
+        ],
+        "should": [],
+        "must_not": []
+        }
+    }
+    }
+    result = es.search(index="billboard", body=QUERY)
+
+    x_values, y_values = [],[]
+    for i in range(len(result["aggregations"]["2"]["buckets"])):
+        aggregations_x = result["aggregations"]["2"]["buckets"][i]['key_as_string']
+        aggregations_y = result["aggregations"]["2"]["buckets"][i]['1']['value']
+        x_values.append(aggregations_x)
+        y_values.append(aggregations_y)
+
+
+    fig = px.line(x=x_values,y=y_values, title=f"Evolution du Average Rank de {query} de au cours des semaines")
+    fig.update_layout(
+        xaxis_title="Dates by Weeks",
+        yaxis_title="Average Rank"
+    )
+    fig.update_yaxes(autorange="reversed")
+    fig_json = fig.to_json()
+
+    return fig_json
+
+##################################################### FLASK APP ##########################################################
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        query = request.form['query']
+        field = request.form.get('field')
+        results = search(query, field)
+        infos = searchinfos(query,field)
+        plot1 = graphsong_rank(query,field)
+        return render_template('index.html', results=results, infos=infos, plot = plot1)
+    else:
+        return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
